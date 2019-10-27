c44735ecac8269dd25b6e39ae5b7f16923b2f35d
dugang <dugang_it@aliyun.com>
2019-10-27 18:38:33 +0800

    Site updated: 2019-10-27 18:38:32

--- js/mouse-click.js
+++ js/mouse-click.js
@@ -1,8 +1,21 @@
 var a_idx = 0;
+
+var a = new Array();
+var f_colors = new Array();
+var scripts = document.getElementsByTagName("script");
+for (var i=0; i< scripts.length;i++) {
+    var script = scripts[i];
+    if (script && script.getAttribute("src") && script.getAttribute("src").indexOf("mouse-click.js")>-1 && script.getAttribute("content")) {
+        a = JSON.parse(script.getAttribute("content"));
+        f_colors = JSON.parse(script.getAttribute("color"));
+    }
+}
+
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
-        var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
+        //var a = new Array("富强", "民主", "文明", "和谐", "自由", "平等", "公正" ,"法治", "爱国", "敬业", "诚信", "友善");
         var $i = $("<span/>").text(a[a_idx]);
+        
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,y = e.pageY;
         $i.css({
@@ -11,7 +24,7 @@ jQuery(document).ready(function($) {
             "left": x,
             "position": "absolute",
             "font-weight": "bold",
-            "color": "#ff6651"});
+            "color": f_colors[a_idx]});
 
         $("body").append($i);
         
